8f64dc3037a3bca8ccc3b8890c88cdab1928daed
xinkle <cswcsy@gmail.com>
2018-04-15 23:43:44 +0900

    Implement resized bitmap download

--- app/src/androidTest/java/io/xinkle/gettyviewer/core/ImageDownloaderTest.kt
+++ app/src/androidTest/java/io/xinkle/gettyviewer/core/ImageDownloaderTest.kt
@@ -11,8 +11,16 @@ class ImageDownloaderTest {
 
     @Test
     fun get_image_from_url_successful() {
-        val imgDownloader = ImageDownloader()
-        val bitmap = imgDownloader.getImageFromUrl("http://www.gettyimagesgallery.com/Images/Thumbnails/1340/134050.jpg")
+        val bitmap = ImageDownloader.getImageFromUrl("http://www.gettyimagesgallery.com/Images/Thumbnails/1340/134050.jpg")
+
+        assertTrue(bitmap != null)
+        Log.d("UnitTest", "Bitmap Height : ${bitmap!!.height}, Width : ${bitmap.width}")
+    }
+
+    @Test
+    fun get_image_from_url_when_resize_successful() {
+        val bitmap = ImageDownloader.getImageFromUrl("http://www.gettyimagesgallery.com/Images/Thumbnails/1340/134050.jpg",
+                50,50)
 
         assertTrue(bitmap != null)
         Log.d("UnitTest", "Bitmap Height : ${bitmap!!.height}, Width : ${bitmap.width}")
--- app/src/main/java/io/xinkle/gettyviewer/core/ImageDownloader.kt
+++ app/src/main/java/io/xinkle/gettyviewer/core/ImageDownloader.kt
@@ -2,13 +2,32 @@ package io.xinkle.gettyviewer.core
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
 
-class ImageDownloader {
-    fun getImageFromUrl(urlString: String): Bitmap? {
+object ImageDownloader {
+    fun getImageFromUrl(urlString: String, width: Int = -1, height: Int = -1): Bitmap? {
         val url = URL(urlString)
+
+        return when {
+            width >= 0 && height >= 0 -> {
+                val bitmapOption = BitmapFactory.Options()
+                bitmapOption.inJustDecodeBounds = true
+
+                BitmapFactory.decodeStream(makeInputStreamFromURL(url), null, bitmapOption)
+
+                bitmapOption.calculateTargetSize(width, height)
+                bitmapOption.inJustDecodeBounds = false
+
+                BitmapFactory.decodeStream(makeInputStreamFromURL(url), null, bitmapOption)
+            }
+            else -> BitmapFactory.decodeStream(makeInputStreamFromURL(url))
+        }
+    }
+
+    private fun makeInputStreamFromURL(url: URL): InputStream {
         val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
 
         connection.apply {
@@ -16,9 +35,9 @@ class ImageDownloader {
             connectTimeout = 7000
             requestMethod = "GET"
             doInput = true
+            connect()
         }
 
-        connection.connect()
-        return BitmapFactory.decodeStream(connection.inputStream)
+        return connection.inputStream
     }
 }
